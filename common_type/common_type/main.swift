77e6f77d2cf5182cdecdfe098262b4003448fc49
chang <chang@1234567899.local>
2017-05-17 15:45:38 +0800

    數字型態

---
+++ common_type/common_type/main.swift
@@ -0,0 +1,22 @@
+//var x:Int  //在６４位元電腦上 預設是Int64
+
+//因為64bit實在太大！在此我用３２位元
+var x:Int32
+
+x = 2147483647
+x = -2147483648
+
+var y:Int64
+
+//y = too big
+
+var z:Int8
+
+z = 127
+z = -128
+
+var a:UInt8
+
+
+a = 255
+//a = -1  => error
